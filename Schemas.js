4623375835737d27cfa1409395469acc559e503d
ytraddan <1nightshelter@gmail.com>
2024-10-31 19:44:26 +0300

    Add Joi back

--- Schemas.js
+++ Schemas.js
@@ -1,41 +1,41 @@
-const Joi = require("joi");
-// const BaseJoi = require("joi");
-// const sanitizeHtml = require("sanitize-html");
+// const Joi = require("joi");
+const BaseJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
 
-// const extension = (joi) => ({
-//   type: "string",
-//   base: joi.string(),
-//   messages: {
-//     "string.escapeHTML": "{{#label}} must not include HTML!",
-//   },
-//   rules: {
-//     escapeHTML: {
-//       validate(value, helpers) {
-//         const clean = sanitizeHtml(value, {
-//           allowedTags: [],
-//           allowedAttributes: {},
-//         });
-//         if (clean !== value) {
-//           return helpers.error("string.escapeHTML", { value });
-//         }
-//         return clean;
-//       },
-//     },
-//   },
-// });
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value) {
+          return helpers.error("string.escapeHTML", { value });
+        }
+        return clean;
+      },
+    },
+  },
+});
 
-// const Joi = BaseJoi.extend(extension);
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
-    title: Joi.string().required(),
-    price: Joi.number().required().min(0),
     image: Joi.string().required(),
-    // title: Joi.string().required().escapeHTML(),
-    // location: Joi.string().required().escapeHTML(),
-    // description: Joi.string().required().escapeHTML(),
-    location: Joi.string().required(),
-    description: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    title: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
+    description: Joi.string().required().escapeHTML(),
+    // title: Joi.string().required(),
+    // location: Joi.string().required(),
+    // description: Joi.string().required(),
   }).required(),
   deleteImages: Joi.array(),
 });
@@ -43,7 +43,7 @@ module.exports.campgroundSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
   review: Joi.object({
     rating: Joi.number().min(1).max(5).required(),
-    // body: Joi.string().required().escapeHTML(),
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
+    // body: Joi.string().required(),
   }).required(),
 });
